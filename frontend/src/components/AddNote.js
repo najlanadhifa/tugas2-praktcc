b4c8f9cce4206e6875b1d03a282a3e2c3850e6dd
unknown <najlanadhifa1103@gmail.com>
2025-03-03 04:58:27 +0700

    tugas prak tcc

---
+++ frontend/src/components/AddNote.js
@@ -0,0 +1,55 @@
+import React, {useState} from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const AddNote = () => {
+    const [nama, setNama] = useState("");
+    const [judul, setJudul] = useState("");
+    const [isi, setIsi] = useState("");
+    const navigate = useNavigate();
+
+    const saveNote = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/notes',{
+                nama,
+                judul, 
+                isi
+            });
+            navigate("/");
+        } catch (error) {
+            console.log(error);        
+        }
+    };
+
+  return (
+    <div className="columns mt-5 is-centered">
+        <div className="columns is-half">
+            <form onSubmit={saveNote}>
+                <div className="field">
+                    <label className="label">Nama</label>
+                    <div className="control">
+                        <input type="text" className="input" value={nama} onChange={(e)=>setNama(e.target.value)} placeholder='Nama'/>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Judul</label>
+                    <div className="control">
+                        <input type="text" className="input" value={judul} onChange={(e)=>setJudul(e.target.value)} placeholder='Judul'/>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Isi</label>
+                    <div className="control">
+                        <input type="text" className="input" value={isi} onChange={(e)=>setIsi(e.target.value)} placeholder='Isi'/>
+                    </div>
+                </div>
+                <div className="field"></div>
+                <button type='submit' className='button is-warning is-dark'>Simpan</button>
+            </form>
+        </div>
+    </div>
+  );
+};
+
+export default AddNote;
